4380af0b1e46db6b65c25220ee7ab8be4607f968
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-07 12:13:19 +0300

    Forming the OAuth2 URL

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -3,5 +3,14 @@ import qs from 'qs'
 const ROOT_URL = 'https://api.imgur.com'
 
 export default {
-  login() {},
+  login() {
+    const querystring = {
+      client_id: process.env.VUE_APP_IMGUR_CLIENT_ID,
+      response_type: 'token',
+    }
+
+    window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
+      querystring,
+    )}`
+  },
 }
