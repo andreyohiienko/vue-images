de9dd4247357e8417938da9c3a5b81db7442c966
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-08 01:27:49 +0300

    Testing images response

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -3,7 +3,14 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'Image list',
+  methods: mapActions(['fetchImages']),
+  created() {
+    //
+    this.fetchImages()
+  },
 }
 </script>
--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -21,3 +21,10 @@ const mutations = {
     state.images = images
   },
 }
+
+export default {
+  state, // names of this of object is important
+  getters,
+  actions,
+  mutations,
+}
