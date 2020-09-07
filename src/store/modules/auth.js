4ba8c1dfb7408df23a5a2cb10adeb67572a913f1
Andrei Ohiienko <andreyohiienko@gmail.com>
2020-09-07 18:16:35 +0300

    Persisting login state

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,7 +1,7 @@
 import api from '../../api/imgur'
 import qs from 'qs'
 
-const state = { token: null }
+const state = { token: window.localStorage.getItem('imgur_token') }
 
 const getters = {
   isLoggedIn: (state) => !!state.token,
@@ -13,7 +13,9 @@ const actions = {
   },
   finalizeLogin({ commit }, hash) {
     const query = qs.parse(hash.replace('#', ''))
+
     commit('setToken', query.access_token)
+    window.localStorage.setItem('imgur_token', query.access_token)
   },
   logout: ({ commit }) => {
     commit('setToken', null)
